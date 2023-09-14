b770be913c54d7df6b420aa890c80d9d97af1b86
xujiachang <chang.xu@northeastern.edu>
2023-09-14 21:36:24 +0900

    Refactored rectangle class

--- monorepo-template-main-main/Lab1/src/rectangle.py
+++ monorepo-template-main-main/Lab1/src/rectangle.py
@@ -5,21 +5,38 @@
  Note that there is no constructor or destructor,
  so a default one will be created for us.
 """
-class Rectangle:
-    def set_values(self, x, y):
-        self.width = x
-        self.height = y
+
+class Shape:
+    def __init__(self, width, height):
+        self._width = width  # Making width and height private
+        self._height = height
+
+    # Getter for width
+    def get_width(self):
+        return self._width
+
+    # Getter for height
+    def get_height(self):
+        return self._height
+
+    # Setter for width
+    def set_width(self, width):
+        self._width = width
+
+    # Setter for height
+    def set_height(self, height):
+        self._height = height
 
     def area(self):
-        return self.width * self.height
+        pass
 
+class Rectangle(Shape):
+    def area(self):
+        return self.get_width() * self.get_height()
 
 if __name__ == "__main__":
     # Create a rectangle object
-    rect = Rectangle()
+    rect = Rectangle(3, 4)
 
-    # Call a member function
-    rect.set_values(3, 4)
-
-    # Print out the area function
+    # Print out the area
     print("area:", rect.area())
